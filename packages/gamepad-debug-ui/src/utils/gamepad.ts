95353f68a7ee0645e3464aa2f351c336258d6dc2
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2022-08-15 15:54:22 -0700

    :zap: fixing and copying utils

---
+++ packages/gamepad-debug-ui/src/utils/gamepad.ts
@@ -0,0 +1,26 @@
+import { createJoymap, createQueryModule, QueryModule } from "joymap"
+
+const MAX_GAMEPADS = 4
+
+const joymap = createJoymap({
+  onPoll() {
+    // console.log("default poll still here")
+  }
+})
+
+// We create "gamepads" to embody each separate gamepad
+// This is different than the "players" we define in Input class
+const gamepads = Array(MAX_GAMEPADS)
+  .fill(0)
+  .map((player, index) => {
+    const module = createQueryModule()
+    joymap.addModule(module)
+
+    return {
+      // We add 1 to index so it starts from Gamepad1
+      name: `Gamepad${index + 1}`,
+      module
+    }
+  })
+
+export { gamepads, joymap }
